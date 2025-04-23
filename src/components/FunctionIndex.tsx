7219a2e4866d4ce9d7e1bb269529f93d0b0f1d3f
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-04-23 14:50:17 +0200

    csv loading function working

--- src/components/FunctionIndex.tsx
+++ src/components/FunctionIndex.tsx
@@ -23,10 +23,12 @@ function FunctionIndex() {
                 return;
             }
 
-            const formData = new FormData();
-            formData.append('file', file);
-
             try {
+                const previous_funs = await FUNCTION_API.searchFunctionsByName(file.name);
+                if (previous_funs.length > 0) {
+                    window.alert(`A file with the name "${file.name}" is already registered.`);
+                    return;
+                }
 
                 const response = await fetch(
                     PYTHON_DAKOTA_BACKEND
@@ -56,37 +58,15 @@ function FunctionIndex() {
                 await FUNCTION_API.createFunction(
                     {
                         "name": file.name,
-                        "type": "",
-                        "url": PYTHON_DAKOTA_BACKEND
-                            + "/flask/retrieve_csv_result"
-                            + "?csv_file_path=" + file.name,
+                        "type": "local.python",
+                        "url": "./examples/csv_retrieval_functions.py:retrieve_csv_result",
                         "description": "",
                         "inputSchema": createSchema(data.input_vars),
-                        "outputSchema": createSchema(data.output_vars)
+                        "outputSchema": createSchema(data.output_vars),
+                        "tags": ["cacheable"],
                     }
                 );
 
-                // const csvData = await file.text();
-                // const rows = csvData.split("\n").map(row => row.split(","));
-                // const headers = rows[0];
-                // const values = rows.slice(1).map(row => {
-                //     return headers.reduce((acc, header, index) => {
-                //         acc[header] = parseFloat(row[index]) || row[index];
-                //         return acc;
-                //     }, {} as Record<string, any>);
-                // });
-
-                // // TODO avoid registering duplicate functions -- should be in the API. But can also control here.
-                // const funs = await FUNCTION_API.searchFunctionsByName(file.name)
-                // const funId = funs[0].id
-                // // console.log("funId: ", funId)
-                // await FUNCTION_API.batchRunFunction({
-                //     functionId: funId,
-                //     jobs: values.map(value => ({
-                //         input: Object.fromEntries(data.input_vars.map(varName => [varName, value[varName]])),
-                //         output: Object.fromEntries(data.output_vars.map(varName => [varName, value[varName]])),
-                //     })),
-                // });
 
                 console.log("Function registered successfully");
                 refreshFunctionList()
@@ -98,6 +78,61 @@ function FunctionIndex() {
         };
 
         inputElement.click();
+    }
+
+    function loadCsvValues() {
+        console.log("start loadCsvValues")
+        const inputElement2 = document.createElement('input');
+        inputElement2.type = 'file';
+        inputElement2.accept = '.csv';
+        inputElement2.click();
+
+        inputElement2.onchange = async (event) => {
+            const file = (event.target as HTMLInputElement).files?.[0];
+            if (!file) {
+                console.error("No file selected");
+                return;
+            }
+            const csvData = await file.text();
+            const rows = csvData.split("\n").map(row => row.split(","));
+            const headers = rows[0];
+            const values = rows.slice(1, 2).map(row => {
+                return headers.reduce((acc, header, index) => {
+                    acc[header] = parseFloat(row[index]) || row[index];
+                    return acc;
+                }, {} as Record<string, any>);
+            });
+
+            // TODO avoid registering duplicate functions -- should be in the API. But can also control here.
+            const funs = await FUNCTION_API.searchFunctionsByName(file.name)
+            if (funs.length !== 1) {
+                console.error(`Expected exactly 1 function, but found ${funs.length}`);
+                return;
+            }
+            const fun = funs[0]
+            console.log("funId: ", fun.id);
+            console.log("fun: ", fun)
+
+            const job = await FUNCTION_API.runFunction(
+                fun.id as number,
+                values.map(value => {
+                    var inputValues = Object.keys(fun.inputSchema.properties).reduce((acc, key) => {
+                        if (key in value) {
+                            acc[key] = value[key];
+                        } else {
+                            console.warn(`Missing value for input key: ${key}`);
+                        }
+                        return acc;
+                    }, {} as Record<string, any>);
+                    inputValues["csv_file_path"] = file.name
+                    console.log(inputValues);
+                    const result = JSON.stringify(inputValues)// Convert input values to a comma-separated string with key-value pairs
+                    return result
+                })[0], // TODO for debugging, just registering first one
+            );
+            console.log(job)
+
+        }
     }
 
     function refreshFunctionList() {
@@ -197,6 +232,7 @@ function FunctionIndex() {
                                 </div> */}
                             </TableCell>
                             <TableCell>{showInputOutputSchema(item.outputSchema)}</TableCell>
+                            <TableCell align='right'>{<Button variant="contained" onClick={() => loadCsvValues()}>Load Values</Button>}</TableCell>
                             <TableCell align='right'>{<Button variant="contained" onClick={() => undefined}>Select</Button>}</TableCell>
                         </TableRow>
                     ))}
