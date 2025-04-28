6dd6e4628ca5c2656f70d82137a61d59826a2192
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-04-28 10:24:25 +0200

    subplots working well - only ylabel is off

--- src/components/PlotDataTogether.tsx
+++ src/components/PlotDataTogether.tsx
@@ -1,49 +1,54 @@
-import React from 'react'
+import React from 'react';
 import Plot from 'react-plotly.js';
 
 type PlotDataType = {
     data: any,
     inputVars: string[],
     qoi: string,
-}
+};
 
 const PlotDataTogether = (props: PlotDataType) => {
-    const { data, inputVars, qoi } = props
+    const { data, inputVars, qoi } = props;
     let plotData = [];
-    for (let i = 0; i < inputVars.length - 1; i++) {
-        plotData.push(
-            {
-                x: data[inputVars[i]].x,
-                y: data[inputVars[i]].y_hat,
-                xaxis: `x${i + 1}`,
-                yaxis: `y`,
-                name: inputVars[i],
-                title: inputVars[i],
-            },
-        )
+    let xAxes = [];
+
+    for (let i = 0; i < inputVars.length; i++) {
+        plotData.push({
+            x: data[inputVars[i]].x,
+            y: data[inputVars[i]].y_hat,
+            name: inputVars[i],
+            xaxis: `x${i + 1}`,
+            yaxis: `y`,
+        });
+
+        xAxes.push({
+            title: { text: inputVars[i] },
+            domain: [i / inputVars.length, (i + 1) * 0.5 / inputVars.length],
+            // for some reason, different x-scales produce different plot sizes? 
+            anchor: 'y',
+            autorange: true,
+            nticks: 4,
+            // tickwidth: 1,
+        });
+
     }
-    console.log(inputVars)
-    console.log(qoi)
+
     let subplot_config = [inputVars.map((_, i) => `x${i + 1}y`)]
-    return (<>
+    return (
         <Plot
             data={plotData}
             layout={{
-                title: { text: qoi },
-                width: 1900,
-                height: 320,
+                // title: { text: qoi },
+                width: 180 * inputVars.length,
+                height: 300,
                 grid: { rows: 1, columns: inputVars.length, subplots: subplot_config, },
+                yaxis: { showgrid: true },
+                ...xAxes.reduce((acc, axis, i) => ({ ...acc, [`xaxis${i + 1}`]: axis }), {}),
                 showlegend: false,
-                // yaxis: {
-                //     range: [0, 10], title: 'Y-axis 1', side: 'left', position: 0, anchor: 'free',
-                //     domain: [0.7, 1.0]
-                // }, // Y-axis 1 with manual position
-                // xaxis: { range: [0, 5], domain: [0.1, 0.9] }, // Adjust x-axis domain to make space for the y-axis text
-
             }}
             config={{ responsive: true }}
         />
-    </>)
-}
+    );
+};
 
-export default PlotDataTogether
+export default PlotDataTogether;
