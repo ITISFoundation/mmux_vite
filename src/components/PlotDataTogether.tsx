b83473e9f0491f3af0d7873f6f702ac2241659a1
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-04-28 09:43:23 +0200

    managed shared Y axis but looking bad

--- src/components/PlotDataTogether.tsx
+++ src/components/PlotDataTogether.tsx
@@ -4,30 +4,46 @@ import Plot from 'react-plotly.js';
 type PlotDataType = {
     data: any,
     inputVars: string[],
+    qoi: string,
 }
 
 const PlotDataTogether = (props: PlotDataType) => {
-    const { data, inputVars } = props
+    const { data, inputVars, qoi } = props
     let plotData = [];
     for (let i = 0; i < inputVars.length - 1; i++) {
         plotData.push(
             {
                 x: data[inputVars[i]].x,
                 y: data[inputVars[i]].y_hat,
-                type: 'scatter',
                 xaxis: `x${i + 1}`,
-                yaxis: `y${i + 1}`,
+                yaxis: `y`,
                 name: inputVars[i],
+                title: inputVars[i],
             },
         )
     }
-
-    return <>
+    console.log(inputVars)
+    console.log(qoi)
+    let subplot_config = [inputVars.map((_, i) => `x${i + 1}y`)]
+    return (<>
         <Plot
             data={plotData}
-            layout={{ width: 900, height: 620, title: { text: 'General Plot' }, grid: { rows: 4, columns: 3, pattern: 'independent' } }}
+            layout={{
+                title: { text: qoi },
+                width: 1900,
+                height: 320,
+                grid: { rows: 1, columns: inputVars.length, subplots: subplot_config, },
+                showlegend: false,
+                // yaxis: {
+                //     range: [0, 10], title: 'Y-axis 1', side: 'left', position: 0, anchor: 'free',
+                //     domain: [0.7, 1.0]
+                // }, // Y-axis 1 with manual position
+                // xaxis: { range: [0, 5], domain: [0.1, 0.9] }, // Adjust x-axis domain to make space for the y-axis text
+
+            }}
+            config={{ responsive: true }}
         />
-    </>
+    </>)
 }
 
 export default PlotDataTogether
