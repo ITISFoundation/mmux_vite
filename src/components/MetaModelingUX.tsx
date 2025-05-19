7d4b73a59e467f515e32e9477ad8bf40afbfe01e
odeimaiz <maiz@itis.swiss>
2025-05-19 14:37:01 +0200

    less css

--- src/components/MetaModelingUX.tsx
+++ src/components/MetaModelingUX.tsx
@@ -1,11 +1,10 @@
 import { Card } from "@mui/material";
-import "./MetaModelingUX.css";
 import Header from "./Header";
 
 
 export default function MetaModelingUX(props) {
     return (
-        <Card variant="plain" className="MMUX-tab">
+        <Card variant="plain">
             <Header tabTitle={props.tabTitle} headerType={props.headerType} />
             <Card variant="plain" sx={{margin:"15px"} } raised={false} children={props.children}/>
         </Card>
