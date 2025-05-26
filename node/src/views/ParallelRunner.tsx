7f433402188350f5829393d472e1313a6df1ef62
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-05-26 14:41:09 +0200

    ParallelRunner only shows jobs from running JobCollection

--- node/src/views/ParallelRunner.tsx
+++ node/src/views/ParallelRunner.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback, useContext } from 'react'
 import type { FunctionJob } from '../osparc-api-ts-client';
 import MMUXContext from './MMUXContext';
-import { getFunctionJobsFromFunctionUid } from '../components/function_utils';
+import { getFunctionJob, getFunctionJobsFromFunctionJobCollection } from '../components/function_utils';
 
 const statusColors = {
     PENDING: 'bg-gray-300',
@@ -190,9 +190,17 @@ export function Dashboard(props: JobDashboardProps) {
     const fetchJobs = useCallback(async () => {
         try {
             // TODO get jobs for JobCollection (instead of all related to that function) when Werner exposes that
-            let jobList = await getFunctionJobsFromFunctionUid(context?.selectedFunction?.uid as string);
+            let jobList = await getFunctionJobsFromFunctionJobCollection(context?.runningJobCollection?.uid as string);
+            // let jobList = context?.runningJobCollection?.jobIds?.map(async (job_uid) => {
+            //     return await getFunctionJob(job_uid)
+            // }
+            // )
             console.log("Fetched jobs:", jobList);
-            setJobs(jobList);
+            if (jobList !== undefined) {
+                setJobs(jobList);
+            } else {
+                // TODO add warning to be displayed
+            }
             // setTotalETA(data.totalETA);
             setLoading(false);
         } catch (e) {
