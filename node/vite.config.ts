bdbb9fe483ba6406e034926dbd7d97abfe40bc48
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-05-30 11:49:09 +0200

    changed node port to 8888 to avoid conflict with Splashtop (remote desktop service)

--- node/vite.config.ts
+++ node/vite.config.ts
@@ -11,14 +11,14 @@ export default defineConfig({
     tailwindcss(),
   ],
   preview: {
-    port: 8080,
+    port: 8888,
     strictPort: true,
   },
   server: {
-    port: 8080,
+    port: 8888,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:8080",
+    origin: "http://0.0.0.0:8888",
   },
   test: defineVitestConfig({
     globals: true,
