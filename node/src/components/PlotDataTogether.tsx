9101f480967d60d4756bda1cf48da2f755abeb21
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-06-02 13:20:19 +0200

    refactoring plots

--- node/src/components/PlotDataTogether.tsx
+++ node/src/components/PlotDataTogether.tsx
@@ -1,37 +1,67 @@
+import { useContext, useState } from 'react';
 import Plot from 'react-plotly.js';
+import MMUXContext from '../views/MMUXContext';
+import { PYTHON_DAKOTA_BACKEND } from '../utils/api_objects';
+import { FunctionJob } from '../osparc-api-ts-client/models/FunctionJob';
 
-type PlotDataType = {
-    data: any,
-    inputVars: string[],
-    qoi: string,
-};
-
-const PlotDataTogether = (props: PlotDataType) => {
-    console.log("PlotDataTogether Props", props)
-    const { data, inputVars, qoi } = props;
-    console.log("PlotDataTogether data", data)
-    console.log("PlotDataTogether inputVars", inputVars)
-    console.log("PlotDataTogether qoi", qoi)
+type PredictedData = {
+    x: ;
+    y: ;
+    y_hat: ;
+    std: ;
+}
+type PlotInputData = {
+    jobs: FunctionJob[]
+}
+const Curves1DPlots = (props: PlotInputData) => {
+    const { jobs } = props
+    const context = useContext(MMUXContext);
+    const [data, setDataSumoCentralCurves] = useState(undefined)
+    console.log("InputVars to 1D curves: ", context?.inputVars)
+    console.log("QoI to 1D curves: ", context?.selectedQoI)
     let plotData = [];
     let xAxes = [];
 
-    if (data === undefined || inputVars === undefined) {
+    // TODO move this to the PlotComponent (same as the 2D and 3D plots)
+    async function RunPlotCentralSuMoInterpolations() {
+        console.log("Evaluating SuMo for 1D curves...");
+        fetch(
+            PYTHON_DAKOTA_BACKEND + '/flask/sumo_along_axes',
+            {
+                method: "POST",
+                body: JSON.stringify(
+                    {
+                        inputs: context?.inputVars,
+                        output: context?.selectedQoI,
+                        FunctionJobs: jobs, // TODO bfr this was UIDs, now it is the full job info
+                        log: false,
+                    }
+                ),
+            }).then(function (response) {
+                return response.json()
+            }).then(function (data) {
+                setDataSumoCentralCurves(data)
+            }).catch(error => console.debug('Error:', error));
+    }
+    // TODO run sumo here, to get data. Use similar workflow as I was doing before.
+
+    if (data === undefined || context?.inputVars === undefined) {
         return <span>Loading...</span>;
     } else {
-        for (let i = 0; i < inputVars.length; i++) {
+        for (let i = 0; i < context?.inputVars.length; i++) {
             plotData.push({
-                x: data[inputVars[i]].x,
-                y: data[inputVars[i]].y_hat,
-                name: inputVars[i],
+                x: data[context?.inputVars[i]].x,
+                y: data[context?.inputVars[i]].y_hat,
+                name: context?.inputVars[i],
                 xaxis: `x${i + 1}`,
                 yaxis: `y`,
             });
-    
-            const w = 1 / inputVars.length
+
+            const w = 1 / context?.inputVars.length
             const padding = 0.2 // this means 20% of each figure size
             const domain = [i * w + padding / 2 * w, (i + 1) * w - padding / 2 * w]
             xAxes.push({
-                title: { text: inputVars[i] },
+                title: { text: context?.inputVars[i] },
                 domain: domain,
                 // for some reason, different x-scales produce different plot sizes? 
                 anchor: 'y',
@@ -39,26 +69,26 @@ const PlotDataTogether = (props: PlotDataType) => {
                 nticks: 4,
             });
         }
-    
-        let subplot_config = [inputVars.map((_, i) => `x${i + 1}y`)]
+
+        let subplot_config = [context?.inputVars.map((_, i) => `x${i + 1}y`)]
         return <Plot
-                data={plotData}
-                layout={{
-                    // title: { text: qoi },
-                    width: 180 * inputVars.length,
-                    height: 300,
-                    grid: { rows: 1, columns: inputVars.length, subplots: subplot_config },
-                    yaxis: {
-                        // title: { text: qoi }, 
-                        showgrid: true, anchor: 'x1'
-                    },
-                    ...xAxes.reduce((acc, axis, i) => ({ ...acc, [`xaxis${i + 1}`]: axis }), {}),
-                    showlegend: false,
-                }}
-                config={{ responsive: true }}
-            />
+            data={plotData}
+            layout={{
+                // title: { text: qoi },
+                width: 180 * context?.inputVars.length,
+                height: 300,
+                grid: { rows: 1, columns: context?.inputVars.length, subplots: subplot_config },
+                yaxis: {
+                    // title: { text: qoi }, 
+                    showgrid: true, anchor: 'x1'
+                },
+                ...xAxes.reduce((acc, axis, i) => ({ ...acc, [`xaxis${i + 1}`]: axis }), {}),
+                showlegend: false,
+            }}
+            config={{ responsive: true }}
+        />
 
     }
 };
 
-export default PlotDataTogether;
+export default Curves1DPlots;
