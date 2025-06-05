bb5488a7ed57b8a8dc6a8c9bb1998153daa277ed
Alejandro Parcet <parcet@speag.com>
2025-06-05 15:22:05 +0200

    fix: made compiler happy
    Signed-off-by: Alejandro Parcet <parcet@speag.com>

--- node/src/views/JobSetup.tsx
+++ node/src/views/JobSetup.tsx
@@ -3,13 +3,28 @@ import { useMMUXContext } from "../context/MMUXContext";
 import PlusButton from "../components/PlusButton";
 import { Sampling } from "../components/Sampling";
 import JobSelector from "../components/JobSelector";
+import { useState, useRef } from "react";
 
 export default function Setup() {
   const { selectedFunction } = useMMUXContext();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [progress, setProgress] = useState<number>(0);
+  const [jobProgress, setJobProgress] = useState<number>(0);
+  const jobsFetched = useRef(0);
+  const colsFetched = useRef(0);
 
   return (
     <MetaModelingUX tabTitle="Base Function Selection" headerType="setup">
-      <JobSelector />
+      <JobSelector
+        loading={loading}
+        setLoading={setLoading}
+        jobProgress={jobProgress}
+        setJobProgress={setJobProgress}
+        progress={progress}
+        setProgress={setProgress}
+        colsFetched={colsFetched}
+        jobsFetched={jobsFetched}
+      />
       {selectedFunction !== undefined ? (
         <PlusButton
           onClickFun={() => null}
