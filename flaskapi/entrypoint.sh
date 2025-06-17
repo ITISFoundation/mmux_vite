53a69b56734520cf0d61670b20f557783ee61d8a
Andrei Neagu <neagu@itis.swiss>
2025-06-17 09:23:37 +0200

    fixed development mode

--- flaskapi/entrypoint.sh
+++ flaskapi/entrypoint.sh
@@ -20,6 +20,9 @@ export LOG_LEVEL=${LOG_LEVEL:-INFO}
 if [ "$DEVELOPMENT_MODE" = "true" ]; then
   # Development mode - use Flask's built-in server
 
+  # copy library to application directory
+  cp -R /mmux_python /app/mmux_python
+
   export FLASK_APP=flask_workflows.py
   export FLASK_DEBUG=1
   FLASK_ARGS=("--host=$HOST" "--port=$PORT" "--debug" "--debugger" "--reload")
