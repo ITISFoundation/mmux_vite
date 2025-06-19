1ed8a80cdee3cbb8ea44d31c6619a6574e840bcb
Alejandro Parcet <parcet@speag.com>
2025-06-19 09:53:41 +0200

    fix: improved legibility of MMUX
    Signed-off-by: Alejandro Parcet <parcet@speag.com>

--- node/src/components/navigation/MetaModelingUX.tsx
+++ node/src/components/navigation/MetaModelingUX.tsx
@@ -3,23 +3,35 @@ import Header from "./Header";
 import StyledHyperLink from "../HyperLink";
 
 const MetaCard = styled(Card)`
-padding: 16px;
-border-radius: 16px;
-`
+  padding: 16px;
+  border-radius: 16px;
+`;
 
 export default function MetaModelingUX(props: MetaModelingUXProps) {
-    const { children, tabTitle, infoText, ExtendedInfoText, headerType } = props;
-    return (
-        <MetaCard variant="outlined">
-            <Header tabTitle={tabTitle} infoText={infoText} ExtendedInfoText={ExtendedInfoText} headerType={headerType}
-                helpContents={(<>
-                    For more information, please see the
-                    <StyledHyperLink text="Tutorial" link="https://github.com/.../main/Tutorial.md" />
-                    and the
-                    <StyledHyperLink text="Manual" link="https://github.com/.../main/Manual.md" />
-                </>)}
+  const { children, tabTitle, infoText, ExtendedInfoText, headerType } = props;
+  return (
+    <MetaCard variant="outlined">
+      <Header
+        tabTitle={tabTitle}
+        infoText={infoText}
+        ExtendedInfoText={ExtendedInfoText}
+        headerType={headerType}
+        helpContents={
+          <>
+            For more information, please see the
+            <StyledHyperLink
+              text="Tutorial"
+              link="https://github.com/.../main/Tutorial.md"
             />
-            {children}
-        </MetaCard>
-    );
+            and the
+            <StyledHyperLink
+              text="Manual"
+              link="https://github.com/.../main/Manual.md"
+            />
+          </>
+        }
+      />
+      {children}
+    </MetaCard>
+  );
 }
