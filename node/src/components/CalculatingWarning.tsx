e453c36dfbd5e5a17d6eb2d0b9c384c6256236ab
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-06-20 20:28:30 +0200

    improve calculating warning (allow on-the-fly updates)

--- node/src/components/CalculatingWarning.tsx
+++ node/src/components/CalculatingWarning.tsx
@@ -2,13 +2,16 @@ import { DisplayMessage } from "./DisplayMessage";
 
 type CalculatingWarningPropsType = {
   height?: number;
+  dontShowText?: boolean;
 }
 
 // Insert if calculating == true
 const CalculatingWarning = (props: CalculatingWarningPropsType) => {
-  const { height } = props
+  const { height, dontShowText } = props
+  console.log("Dont Show Text: ", dontShowText)
+  console.log("Text: ", dontShowText ? "" : "Calculating...")
   return (
-    <DisplayMessage mssg={"Calculating..."} height={height} />
+    <DisplayMessage mssg={dontShowText ? "" : "Calculating..."} height={height} />
   )
 }
 
