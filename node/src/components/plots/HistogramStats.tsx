9e65fc39d37bf53064eea7f762a0a149b337a04e
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-06-23 18:46:54 +0200

    fix: include UQ histogram metrics pt2

--- node/src/components/plots/HistogramStats.tsx
+++ node/src/components/plots/HistogramStats.tsx
@@ -1,11 +1,11 @@
-import { Typography, Box, useTheme } from "@mui/material";
-import Header from "../navigation/Header";
+import { Box, useTheme } from "@mui/material";
+import Metric from "./Metric";
+import MetricRow from "./MetricRow";
 
 const HistogramStats = (props: dataUQHistogramType) => {
   const theme = useTheme();
-  const { q1, median, q3, whisker_min, whisker_max } = props;
+  const { mean, std, min, max } = props;
 
-  // TODO update this with the Metric / MEtricROw component
   return (
     <Box width="100%" display="flex" justifyContent="left">
       <Box
@@ -15,34 +15,24 @@ const HistogramStats = (props: dataUQHistogramType) => {
           textAlign: "left",
         }}
       >
-        <Header headerType="subTitle" infoText="" tabTitle="Summary Statistics" />
-        <ul style={{ listStyleType: "initial", marginLeft: "24px" }}>
-          <li>
-            <Typography variant="body1" fontFamily={"inherit"} fontWeight={100}>
-              <strong>Median:</strong> {median.toPrecision(4)}
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="body1" fontFamily={"inherit"} fontWeight={100}>
-              <strong>Q1:</strong> {q1.toPrecision(4)}
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="body1" fontFamily={"inherit"} fontWeight={100}>
-              <strong>Q3:</strong> {q3.toPrecision(4)}
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="body1" fontFamily={"inherit"} fontWeight={100}>
-              <strong>Whisker Min:</strong> {whisker_min.toPrecision(4)}
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="body1" fontFamily={"inherit"} fontWeight={100}>
-              <strong>Whisker Max:</strong> {whisker_max.toPrecision(4)}
-            </Typography>
-          </li>
-        </ul>
+        <MetricRow width={1080}>
+          <Metric
+            metricName={"Mean"}
+            metricValue={mean}
+          />
+          <Metric
+            metricName={"Std"}
+            metricValue={std}
+          />
+          <Metric
+            metricName={"Min"}
+            metricValue={min}
+          />
+          <Metric
+            metricName={"Max"}
+            metricValue={max}
+          />
+        </MetricRow>
       </Box>
     </Box>
   );
