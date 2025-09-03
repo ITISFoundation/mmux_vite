4619af7f1fcd5e4039550be8937542c8d02396bc
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-09-03 13:59:07 +0200

    FIX: wip try to use uv venv for executing flask server

--- flaskapi/entrypoint.sh
+++ flaskapi/entrypoint.sh
@@ -32,7 +32,7 @@ if [ "$DEVELOPMENT_MODE" = "true" ]; then
   FLASK_ARGS=("--host=$HOST" "--port=$PORT" "--debug" "--debugger" "--reload")
 
   echo "$INFO" "Starting Flask development server with arguments: ${FLASK_ARGS[@]}"
-  exec python -m flask run "${FLASK_ARGS[@]}"
+  source ./.venv/bin/activate && echo "$INFO" "Activated virtual environment" && uv run python -m flask run "${FLASK_ARGS[@]}"
 else
   # Production mode - use gunicorn
   echo "$INFO" "Starting gunicorn production server on $HOST:$PORT"
