bca7027dc2d8408d60c80d43c5db60d5abf38c78
Alejandro Parcet <parcet@speag.com>
2025-09-24 10:26:00 +0200

    fix: now when changing rows per page in job table, page is defaulted to 0
    Signed-off-by: Alejandro Parcet <parcet@speag.com>

--- node/src/components/data/JobSelector.tsx
+++ node/src/components/data/JobSelector.tsx
@@ -560,7 +560,10 @@ export default function JobsSelector(props: JobSelectorPropsType) {
                   rowsPerPage={rowsPerPage}
                   page={page}
                   onPageChange={(_e, newPage) => setPage(newPage)}
-                  onRowsPerPageChange={e => setRowsPerPage(parseInt(e.target.value, 10))}
+                  onRowsPerPageChange={e => {
+                    setRowsPerPage(parseInt(e.target.value, 10));
+                    setPage(0);
+                  }}
                 />
               </Box>
             </Card>
