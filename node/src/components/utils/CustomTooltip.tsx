fffcb297c5b575ecb885c5f7d00ac2a7f76c902e
Alejandro Parcet <parcet@speag.com>
2025-09-24 17:36:11 +0200

    fix: locked column width for all JobRows columns and added border to tooltip
    Signed-off-by: Alejandro Parcet <parcet@speag.com>

--- node/src/components/utils/CustomTooltip.tsx
+++ node/src/components/utils/CustomTooltip.tsx
@@ -3,15 +3,25 @@ import { ReactElement, useState } from "react";
 
 interface CustomTooltipProps extends TooltipProps {
   ExtendedTootlip?: ReactElement;
+  border?: boolean;
 }
 
 const CustomTooltip = styled((props: CustomTooltipProps) => {
-  const { ExtendedTootlip, className, title, ...rest } = props;
+  const { ExtendedTootlip, className, title, border, ...rest } = props;
   const theme = useTheme();
   const [open, setOpen] = useState(false);
 
   const infotextWButton = (
-    <Typography variant="body2" fontFamily="inherit">
+    <Typography
+      variant="body2"
+      fontFamily="inherit"
+      sx={{
+        border: border ? `1px solid ${theme.palette.divider}` : undefined,
+        borderRadius: border ? 1 : undefined,
+        padding: border ? "4px" : undefined,
+        margin: border ? "0px" : undefined,
+      }}
+    >
       {title}
       {ExtendedTootlip ? (
         <button
