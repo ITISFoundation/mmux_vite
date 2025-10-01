6a8c14e2ab2bb11cd82f4d0986fb12a420ced510
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-10-01 16:14:22 +0200

    FIX: better error handling for failed Test Job

--- node/src/utils/sampling_utils.ts
+++ node/src/utils/sampling_utils.ts
@@ -34,14 +34,23 @@ export async function runSingleJob(
       console.error("Error running single job: ", error);
     });
   setLaunchingSampling(false);
+  if (!job) {
+    toast.warning("Test Job running failed! Please contact support");
+    return; // Avoid creating copy if runTestJob fails
+  }
   // necessary to make a copy of the test job bcs as of now, the run-job always generates a hidden copy
   // thus, the copy allows the user to see their test run in their dashboard
   // WOuld be nice to be able to abort/delete the TestJob or simply update the run-job endpoint to accept a "hidden" boolean parameter
-  const copyUid: string = (await createJobStudyCopy(selectedFunction?.title as string, job as ProjectFunctionJob)) as string;
-  if (!job) toast.warning("Test Job running failed! Please contact support");
-  else if (!copyUid) toast.warning("Not possible to open your test copy! Please contact support");
-  else if (job.functionClass && job.functionClass === "PROJECT") openStudyUid(copyUid);
-  else toast.warning("Only ProjectFunctionJob can be opened in a new window!");
+  const copyUid = await createJobStudyCopy(selectedFunction?.title as string, job as ProjectFunctionJob);
+  if (copyUid instanceof Error) {
+    toast.warning(`Not possible to open your Job! ${copyUid.message} Please contact support`);
+  } else if (!copyUid) {
+    toast.warning("Not possible to open your Job! Please contact support");
+  } else if (job && job.functionClass && job.functionClass === "PROJECT") {
+    openStudyUid(copyUid as string);
+  } else {
+    toast.warning("Only ProjectFunctionJob can be opened in a new window!");
+  }
 }
 
 /// TODO make this more modular and also include the LHS, Grid, etc
