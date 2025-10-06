4aa811f95757e49cd5fa5823a16ec7b6a080b2d0
alexpargon <alexpargon@gmail.com>
2025-10-06 10:21:12 +0200

    fix: extracted zeroWeight condition and removed React. notation
    Signed-off-by: alexpargon <alexpargon@gmail.com>

--- node/src/context/MOGATableContext.tsx
+++ node/src/context/MOGATableContext.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { createContext, useContext, useEffect, useMemo, useState } from "react";
+import React, { createContext, useContext, useEffect, useMemo, useRef, useState } from "react";
 import { GridSortModel } from "@mui/x-data-grid";
 import { toast } from "react-toastify";
 import { usePersistenceContext } from "./PersistenceContext";
@@ -23,8 +23,9 @@ export function MOGATableContextProvider({ children }: Props) {
   const [localLoading, setLocalLoading] = useState(true);
   const [weights, setWeights] = useState<{ [key: string]: number }>({});
   const [sortModel, setSortModel] = useState<GridSortModel>([]);
-  const weightsToastId = React.useRef<string | number>("");
+  const weightsToastId = useRef<string | number>("");
 
+  const checkAllWeightsZero = (w: { [key: string]: number }) => Object.values(w).every(v => v === 0);
   // Persist only weights and sortModel
   useEffect(() => {
     if (localLoading === true) return; // Avoid saving state while loading
@@ -39,7 +40,7 @@ export function MOGATableContextProvider({ children }: Props) {
 
   useEffect(() => {
     if (!loading && persistence && persistence.currentView !== undefined) {
-      if (Object.values(persistence.weights).every(w => w === 0)) {
+      if (checkAllWeightsZero(persistence.weights)) {
         console.warn("All weights are zero! Resetting to empty object");
         setWeights({});
       } else {
@@ -52,11 +53,11 @@ export function MOGATableContextProvider({ children }: Props) {
 
   useEffect(() => {
     if (!loading) {
-      if (Object.values(weights).every(w => w === 0) && weightsToastId.current === "") {
+      if (checkAllWeightsZero(weights) && weightsToastId.current === "") {
         console.warn("All weights are zero! Setting performance to NaN");
         weightsToastId.current = toast.warning("Not possible to calculate performance - all weights set to zero");
       }
-      if (!Object.values(weights).every(w => w === 0) && weightsToastId.current !== "") {
+      if (!checkAllWeightsZero(weights) && weightsToastId.current !== "") {
         toast.dismiss(weightsToastId.current);
         weightsToastId.current = "";
       }
