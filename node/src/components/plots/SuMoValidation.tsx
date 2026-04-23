85b6a45aa85e39e5b77a549be35af0451e80037a
Javier Garcia Ordonez <ordonez@zmt.swiss>
2026-04-23 14:39:43 +0200

    Fix SuMo validation endpoint path

--- node/src/components/plots/SuMoValidation.tsx
+++ node/src/components/plots/SuMoValidation.tsx
@@ -88,7 +88,7 @@ function SuMoValidation() {
     setPlotData([]);
     setPropagating(true);
 
-    fetch(`${PYTHON_DAKOTA_BACKEND}/flask/dakota/sumo_cross_validation`, {
+    fetch(`/flask/dakota/sumo_cross_validation`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
