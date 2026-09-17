3a13166eed35e6a9563e12d732094eed3ed6fbf5
Javier Garcia Ordonez <ordonez@zmt.swiss>
2026-09-17 16:42:02 +0200

    refactor: share validation modal and QoI selectors

--- node/src/views/MOGA.tsx
+++ node/src/views/MOGA.tsx
@@ -1,8 +1,8 @@
 import { useRef, useState } from "react";
-import SuMoModal from "./SuMoModal";
 import MOGAModal from "./MOGAModal";
+import ValidationModal from "./ValidationModal";
 import MetaModelingUX from "../components/navigation/MetaModelingUX";
-import { OutputSetup } from "./OutputSetup";
+import { Box, Button } from "@mui/material";
 import { JobSampling } from "../components/sampling/JobSampling";
 import { useFunctionContext } from "../context/FunctionContext";
 import { MOGAPareto } from "../components/plots/MOGAPareto";
@@ -19,7 +19,20 @@ export default function MOGA() {
 
   return (
     <MetaModelingUX headerType="title" tabTitle={`Multi Objective Genetic Algorithm: ${selectedFunction?.title}`}>
-      <OutputSetup loading={loading} setSumoModal={setSumoModal} setMogaModal={setMogaModal} mode="moga" />
+      <Box display="flex" justifyContent="space-between" gap={2} padding="0 4px 16px">
+        <Button variant="contained" size="small" disabled={loading || !selectedFunction} onClick={() => setMogaModal(true)}>
+          Optimization Settings
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          disabled={loading || !selectedFunction}
+          onClick={() => setSumoModal(true)}
+          mmux-testid="inspect-model-button"
+        >
+          Inspect Model
+        </Button>
+      </Box>
       <MOGAPareto
         colsFetched={colsFetched}
         jobProgress={jobProgress}
@@ -27,7 +40,7 @@ export default function MOGA() {
         loading={loading}
         setCalculating={setCalculating}
       />
-      <SuMoModal open={sumoModal} setOpen={setSumoModal} />
+      <ValidationModal open={sumoModal} setOpen={setSumoModal} />
       <MOGAModal open={mogaModal} setOpen={setMogaModal} />
       <JobSampling
         loading={loading}
