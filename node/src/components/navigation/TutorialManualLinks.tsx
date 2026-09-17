03e8180d1fb5ee173d8ee33eae55010a8bd960b4
Javier Garcia Ordonez <ordonez@zmt.swiss>
2026-09-17 17:13:35 +0200

    fix(node): use canonical Manual and Tutorial links (#450)

--- node/src/components/navigation/TutorialManualLinks.tsx
+++ node/src/components/navigation/TutorialManualLinks.tsx
@@ -1,73 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { toast, Id } from "react-toastify";
-import { getSimplifiedHost } from "../../utils/functionUtils";
+import React from "react";
 import StyledHyperLink from "../utils/HyperLink";
 
-type HostType = "sim4life" | "osparc" | "unknown";
+export const manualDocsUrl = "https://zurichmedtech.github.io/model-intelligence/manual/hypertool-creation/";
+export const tutorialDocsUrl = "https://zurichmedtech.github.io/model-intelligence/tutorials/overview/";
 
-const checkSimplifiedHost = (): HostType => {
-  const simplifiedHost = getSimplifiedHost();
-  const s4l = simplifiedHost.includes("sim4life");
-  const osparc = simplifiedHost.includes("osparc");
-  if (s4l) return "sim4life";
-  if (osparc) return "osparc";
-  return "unknown";
-};
-
-export function getTutorialLink(): React.ReactNode | undefined {
-  const simplifiedHost = getSimplifiedHost();
-  const check = checkSimplifiedHost();
-  let link: string | undefined;
-  if (check === "sim4life") {
-    link = `https://manual.${simplifiedHost}/manual/Tutorials/MetaModeling/MetaModelingTutorials.html`;
-  } else if (check === "osparc") {
-    link = "https://docs.sparc.science/docs/tutorial-metamodeling-hypertools#/";
-  } else {
-    return <StyledHyperLink text="Tutorials" link={undefined} />;
-  }
-  return <StyledHyperLink text="Tutorials" link={link} />;
+export function getTutorialLink(): React.ReactNode {
+  return <StyledHyperLink text="Tutorials" link={tutorialDocsUrl} />;
 }
 
 export function getManualLink(): React.ReactNode {
-  const check = checkSimplifiedHost();
-  const simplifiedHost = getSimplifiedHost();
-  if (check === "unknown") {
-    return <StyledHyperLink text="Manual" link={undefined} />;
-  }
-  // no specific manual for osparc, refer them to the sim4life manual as well
-  const link = `https://manual.${simplifiedHost}/manual/Manual/MetaModeling/MetaModeling.html`;
-  return <StyledHyperLink text="Manual" link={link} />;
+  return <StyledHyperLink text="Manual" link={manualDocsUrl} />;
 }
 
 type HelpType = "MMHeaderHelp" | "FunctionsHelp";
 
 export function HelpContents({ type }: { type: HelpType }) {
-  const [hostType, setHostType] = useState<HostType | undefined>(undefined);
-  const dismiss = (id: Id) => toast.dismiss({ containerId: id });
-
-  useEffect(() => {
-    if (hostType === undefined) return;
-    const isActive = toast.isActive("HostLinkWarning");
-    if (hostType === "unknown" && isActive === false) {
-      toast("Host could not be detected - links to Tutorials & Manuals will not work", {
-        autoClose: false,
-        closeOnClick: false,
-        pauseOnHover: false,
-        draggable: false,
-        closeButton: true,
-        type: "warning",
-        toastId: "HostLinkWarning",
-      });
-    }
-    if (hostType !== "unknown" && isActive === true) {
-      dismiss("HostLinkWarning");
-    }
-  }, [hostType]);
-
-  useEffect(() => {
-    setHostType(checkSimplifiedHost());
-  }, []);
-
   if (type === "MMHeaderHelp") {
     return (
       <>
